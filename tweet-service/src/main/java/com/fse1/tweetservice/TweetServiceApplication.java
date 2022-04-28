334c663b1f9037af9c53a1daf30ab9f7e8d80640
25samyu <64177020+25samyu@users.noreply.github.com>
2022-04-28 09:17:22 +0530

    Swagger enabled

--- tweet-service/src/main/java/com/fse1/tweetservice/TweetServiceApplication.java
+++ tweet-service/src/main/java/com/fse1/tweetservice/TweetServiceApplication.java
@@ -2,16 +2,28 @@ package com.fse1.tweetservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.fse1.tweetservice" })
 @EnableMongoRepositories
+@EnableSwagger2
 public class TweetServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TweetServiceApplication.class, args);
 	}
 
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.fse1.tweetservice")).build();
+	}
 }

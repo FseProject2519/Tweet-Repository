139197c7d626da17eff406e49a1f92893fbad3f6
25samyu <64177020+25samyu@users.noreply.github.com>
2022-06-16 08:39:12 +0530

    Fix for password-confirm password defect

--- authorization/src/main/java/com/tweetapp/authorization/dto/UserDto.java
+++ authorization/src/main/java/com/tweetapp/authorization/dto/UserDto.java
@@ -8,51 +8,45 @@ import com.tweetapp.authorization.util.EmailConstraint;
 import com.tweetapp.authorization.util.PasswordMatchConstraint;
 import com.tweetapp.authorization.util.UserIdConstraint;
 
-
-@PasswordMatchConstraint.List({ 
-    @PasswordMatchConstraint(
-      password = "password", 
-      confirmPassword = "confirmPassword", 
-      message = "Passwords do not match!"
-    )})
+@PasswordMatchConstraint.List({
+		@PasswordMatchConstraint(password = "password", confirmPassword = "confirmPassword", message = "Password and Cofirmed password do not match") })
 public class UserDto {
 
-	
 	private String id;
-	
+
 	@NotBlank(message = "Username is mandatory, please provide a valid username")
 	@Size(min = 8, max = 30, message = "Username should be of  8 to 30 characters")
 	@Pattern(regexp = "^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]", message = "Please enter a valid username")
 	@UserIdConstraint
 	private String userId;
-	
+
 	@NotBlank(message = "Password is mandatory, please provide a valid password")
 	@Size(min = 8, max = 20, message = "Password should be of  8 to 20 characters")
 	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}", message = "Please enter a valid password")
 	private String password;
-	
+
 	@NotBlank(message = "Password confirmation is mandatory, please confirm your password")
 	private String confirmPassword;
-	
+
 	@NotBlank(message = "First name is mandatory, please provide a valid first name")
-	@Size(min=2)
-	@Pattern(regexp="^[a-zA-Z]*",message="Please enter a valid first name")
+	@Size(min = 2)
+	@Pattern(regexp = "^[a-zA-Z]*", message = "Please enter a valid first name")
 	private String firstName;
-	
+
 	@NotBlank(message = "Last name is mandatory, please provide a valid last name")
-	@Size(min=1)
-	@Pattern(regexp="^[a-zA-Z]*",message="Please enter a valid last name")
+	@Size(min = 1)
+	@Pattern(regexp = "^[a-zA-Z]*", message = "Please enter a valid last name")
 	private String lastName;
-	
+
 	@NotBlank(message = "Email is mandatory, please provide a valid email")
-	@Pattern(regexp="^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}",message="Please enter a valid email id")
+	@Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}", message = "Please enter a valid email id")
 	@EmailConstraint
 	private String email;
-	
+
 	@NotBlank(message = "Contact number is mandatory, please provide a valid contact number")
-	@Pattern(regexp="^[0-9]{10}",message="Please enter a 10 digit contact number")
+	@Pattern(regexp = "^[0-9]{10}", message = "Please enter a 10 digit contact number")
 	private String contactNumber;
-	
+
 	public String getId() {
 		return id;
 	}
@@ -116,6 +110,5 @@ public class UserDto {
 	public void setContactNumber(String contactNumber) {
 		this.contactNumber = contactNumber;
 	}
-
 
 }
